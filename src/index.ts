8496c6654723a1839d63ba4c68edc09ebb52304d
Neeraj-Palliyali <palliyali.neeraj.com>
2023-06-20 01:51:35 +0100

    basic orm - ie reddit post

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello world")
+import { MikroORM, RequiredEntityData } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+
+    const orm = await MikroORM.init(
+        microConfig
+    );
+    await orm.getMigrator().up();
+    const fork = orm.em.fork();
+    const post = fork.create(Post, { title: "My first Post" } as RequiredEntityData<Post>);
+    await fork.persistAndFlush(post);
+}
+
+
+main().catch((err) => {
+    console.error(err)
+});
