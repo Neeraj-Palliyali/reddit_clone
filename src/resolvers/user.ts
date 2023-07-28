301a143bef4936dcf283ef31db5853e3900c5de5
neeraj <palliyali.neeraj@gmail.com>
2023-07-28 18:03:30 +0100

    login with if validations

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,9 +1,29 @@
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
-import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, Mutation, ObjectType, Resolver } from "type-graphql";
 import argon2 from "argon2";
 import { UsernamePasswordInput } from "./usernamePasswordValidator";
+import { MaxLength } from "class-validator";
 
+@ObjectType()
+class FieldError {
+
+    @Field()
+    @MaxLength(30)
+    field: string;
+    
+    @Field()
+    @MaxLength(300)
+    message: string;
+}
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], { nullable: true })
+    errors?: FieldError[];
+
+    @Field(() => User, { nullable: true })
+    user?: User;
+}
 @Resolver()
 export class UserResolver {
 
@@ -19,5 +39,53 @@ export class UserResolver {
         });
         await ctx.em.persistAndFlush(user);
         return user;
+    }
+
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg('options') options: UsernamePasswordInput,
+        @Ctx() ctx: MyContext
+    ): Promise<UserResponse> {
+
+        if (options.username.length <= 2){
+            return {
+                errors: [{
+                    field: 'username',
+                    message: "Username too short to be valid",
+                },]
+            }
+        }
+
+        if (options.password.length <= 2){
+            return {
+                errors: [{
+                    field: 'password',
+                    message: "password too short to be valid",
+                },]
+            }
+        }
+        const user = await ctx.em.findOneOrFail(User, {
+            username: options.username,
+        });
+        
+        if (!user) {
+            return {
+                errors: [{
+                    field: 'username',
+                    message: "Username does not exist",
+                },]
+            }
+        }
+        const valid = await argon2.verify(user.password, options.password);
+        if (!valid) {
+            return {
+                errors: [{
+                    field: 'password',
+                    message: "Wrong password",
+                },]
+            }
+        }
+
+        return { user: user };
     }
 }
